4d838b091dd61097e8e6cd76e796d395fdb8af56
garbanzos <yiyant@gmail.com>
2017-09-25 13:59:49 +0800

    initial commit

---
+++ L4-ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[8];
+
+	printf("a[9] %d\n", a[9]);
+	return 0;
+}
---
+++ L4-ex3.c
@@ -0,0 +1,30 @@
+/*
+Name: Tan Yi Yan
+Matric. no.: A0127051U
+Email: yiyan@u.nus.edu
+CS4239 Lab 4 Exercise 3
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	int choice=1;
+
+	if (argc > 1) choice=atoi(argv[1]);
+	printf("choice %d\n", choice);
+
+    switch (choice) {
+    case 1:
+        // some error
+        break;
+    case 2:
+        // some error
+        break;
+    case 3:
+        // some error
+        break;
+    }
+    return 0;
+}
