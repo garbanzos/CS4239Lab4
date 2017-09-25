e88d94f269e8493e31eb6ebe210f1faa6c090ee1
garbanzos <yiyant@gmail.com>
2017-09-25 16:45:53 +0800

    removed a random s in the comment

--- L4-ex2.c
+++ L4-ex2.c
@@ -8,29 +8,29 @@ CS4239 Lab 4 Exercise 2
 #include <stdlib.h>
 #include <stdio.h>
 
-int global[30] = {0};  
+int global[30] = {0};
 
 int main(int argc, char *argv[], char *envp[]) {
-	int choice = 1, *p; 
-	int buffer[10]; 
+	int choice = 1, *p;
+	int buffer[10];
 
 	if (argc > 1) choice=atoi(argv[1]);
 	printf("choice %d\n", choice);
 
     switch (choice) {
     case 1:
-        // use after free s
-		p = malloc(sizeof(int)); 
-		free(p); 
+        // use after free 
+		p = malloc(sizeof(int));
+		free(p);
 		*p = 100;
         break;
     case 2:
-        // stack buffer overflow 
-		buffer[22] = 200; 
+        // stack buffer overflow
+		buffer[22] = 200;
         break;
     case 3:
         // global buffer overflow
-		global[33] = 300; 
+		global[33] = 300;
         break;
     }
     return 0;
